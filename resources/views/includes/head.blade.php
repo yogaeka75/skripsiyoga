821214ad45605932aac8fee6027a47cbdd399c99
Yogaeka755 <ekayoga.ye21@gmail.com>
2024-12-28 23:44:27 +0700

    add script maze

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -42,4 +42,25 @@
     <!-- Select2 -->
     <link rel="stylesheet" href="{{ url('/plugins/select2/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ url('/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <script>
+(function (m, a, z, e) {
+  var s, t;
+  try {
+    t = m.sessionStorage.getItem('maze-us');
+  } catch (err) {}
+
+  if (!t) {
+    t = new Date().getTime();
+    try {
+      m.sessionStorage.setItem('maze-us', t);
+    } catch (err) {}
+  }
+
+  s = a.createElement('script');
+  s.src = z + '?apiKey=' + e;
+  s.async = true;
+  a.getElementsByTagName('head')[0].appendChild(s);
+  m.mazeUniversalSnippetApiKey = e;
+})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '53874ff9-b9cd-4f94-be19-3845d7cdee5e');
+</script>
 </head>
--- resources/views/pages/landingpage/product-detail.blade.php
+++ resources/views/pages/landingpage/product-detail.blade.php
@@ -23,6 +23,27 @@
             background-color: #000;
         }
     </style>
+    <script>
+(function (m, a, z, e) {
+  var s, t;
+  try {
+    t = m.sessionStorage.getItem('maze-us');
+  } catch (err) {}
+
+  if (!t) {
+    t = new Date().getTime();
+    try {
+      m.sessionStorage.setItem('maze-us', t);
+    } catch (err) {}
+  }
+
+  s = a.createElement('script');
+  s.src = z + '?apiKey=' + e;
+  s.async = true;
+  a.getElementsByTagName('head')[0].appendChild(s);
+  m.mazeUniversalSnippetApiKey = e;
+})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '53874ff9-b9cd-4f94-be19-3845d7cdee5e');
+</script>
 </head>
 
 <body>
